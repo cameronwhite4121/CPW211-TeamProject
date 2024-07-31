ec2465d15d13a44d3057772a998c7449d3d96c3b
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-30 23:30:16 -0700

    Added some initial classes such as Character and weapon, also installed some NuGet packages to handle Databases

---
+++ CPW211-TeamProject/Models/character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW211_TeamProject.Models
+{
+    /// <summary>
+    /// This class represents a character, be it a villain, hero or side character.
+    /// </summary>
+    public partial class character
+    {
+        public string Name { get; set; }
+
+        public string Alias { get; set; }
+
+        public string SuperPower { get; set; }
+
+        public virtual ICollection<weapon> Weapon { get; set; }
+
+        public string ComicBookDebut { get; set; }
+
+        public DateTime DebutDate { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
