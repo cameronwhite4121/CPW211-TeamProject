a8e117209d705d10aac72454548f6aa2d1fef646
Cameron White <cameronwhite4121@gmail.com>
2024-07-31 21:24:48 -0700

    Added form controls to the main form

--- CPW211-TeamProject/Form1.Designer.cs
+++ CPW211-TeamProject/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(25, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(452, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Marvel/Dc Character Index";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(123, 102);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(253, 248);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.Location = new Point(304, 396);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 43);
+            button2.TabIndex = 3;
+            button2.Text = "View/Delete Character";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.Location = new Point(76, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 43);
+            button1.TabIndex = 4;
+            button1.Text = "Add Character";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 497);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Character Index";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Button button1;
     }
 }
