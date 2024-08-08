f759a79520913ffa2bc6defe9d540ee7f39077fb
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-07 18:15:00 -0700

    Refactor project structure and update Character class Removed Data folder. Enhanced Character class with data annotations for validation. Added Age and Rival properties; removed ImageUrl. Deleted the Weapon class.

--- CPW211-TeamProject/Models/character.cs
+++ CPW211-TeamProject/Models/character.cs
@@ -1,28 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPW211_TeamProject.Models
 {
-    /// <summary>
-    /// This class represents a character, be it a villain, hero or side character.
-    /// </summary>
-    public partial class character
+    /// <summary> 
+    /// This class represents a character, be it a villain, hero or side character. 
+    /// </summary> 
+    public partial class Character
     {
-        public string Name { get; set; }
+        /// <summary> 
+        /// Character's ID as they are stored in the database. 
+        /// </summary> 
+        [Key] // Tell EF Core that this is the primary key 
+        private int _id;
 
-        public string Alias { get; set; }
+        /// <summary> 
+        /// Character's name. 
+        /// </summary> 
+        [Required]
+        public required string Name { get; set; }
 
-        public string SuperPower { get; set; }
+        /// <summary> 
+        /// Character's age, the int max value is 2,147,483,647. 
+        /// </summary> 
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Age { get; set; }
 
-        public virtual ICollection<weapon> Weapon { get; set; }
+        /// <summary> 
+        /// Character's Superpower, it can be left empty 
+        /// in case they do not have one. 
+        /// </summary> 
+        public string? SuperPower { get; set; }
 
-        public string ComicBookDebut { get; set; }
+        /// <summary> 
+        /// Character's Rival, it can be left empty, 
+        /// or they can add multiples 
+        /// </summary> 
+        [StringLength(1000)]
+        public string? Rival { get; set; }
 
+        /// <summary> 
+        /// Character's debut date, using DateTime 
+        /// </summary> 
+        [Required]
         public DateTime DebutDate { get; set; }
-
-        public string ImageUrl { get; set; }
+        
+        /// <summary> 
+        /// Character's comic book issue debut 
+        /// for example, Spiderman: "Amazing Fantasy #15" 
+        /// </summary> 
+        [Required]
+        [StringLength(100)]
+        public required string ComicBookDebut { get; set; }
     }
 }
